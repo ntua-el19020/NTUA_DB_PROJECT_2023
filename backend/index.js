4397e96a1286718bf16b9313eefb76411c576c92
ntua-el19120 <115192395+ntua-el19120@users.noreply.github.com>
2023-06-04 19:55:37 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,11 +26,6 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // Serve the login page
-
-app.get('/libq', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/startpage.html'));
-});
-
 app.get('/libq/login', function (req, res) {
   res.sendFile(path.join(__dirname, '../frontend/login.html'));
 });
@@ -45,6 +40,10 @@ app.get('/libq/user/checkbooks1', (req, res) => {
 
 app.get('/libq/schooladmin/viewbooks1', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/viewbooks.html'));
+});
+
+app.get('/libq/schooladmin/meanratings1', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/meanratings.html'));
 });
 
 app.get('/libq/generaladmin', function (req, res) {
@@ -119,6 +118,9 @@ app.use('/libq/login', login);
 
 const addbook = require('./SchoolAdmin/addbook.js');
 app.use('/libq/addbook', addbook);
+
+const meanrating = require('./SchoolAdmin/meanratings.js');
+app.use('/libq/schooladmin/meanratings', meanrating);
 
 const chgpaswd = require('./GeneralAdmin/changepassword.js');
 app.use('/libq/generaladmin/changepassword', chgpaswd);
@@ -247,6 +249,8 @@ const create2book = require('./SchoolAdmin/createbook2.js');
 app.use('/libq/createbook2',create2book);
 
 
+
+
 const password = require('./user/changepassword.js');
 app.use('/libq/user/changepassword',password);
 
@@ -256,12 +260,6 @@ app.use('/libq/user/updatedata',updatedata);
 
 const viewdata = require('./user/viewdata.js');
 app.use('/libq/user/viewdata', viewdata);
-
-const approveUser = require('./SchoolAdmin/approveUser.js');
-app.use('/libq/schooladmin/approveuser', approveUser);
-
-const confirmApprove = require('./SchoolAdmin/confirmapprove.js');
-app.use('/libq/schooladmin/approve', confirmApprove);
 
 app.get('/libq/schooladmin/deleteuser1', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/deleteuser.html'));
@@ -283,6 +281,26 @@ app.post('/delete-user', (req, res) => {
   });
 });
 
+
+app.get('/libq/schooladmin/deactivateuser1', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/deactivateuser.html'));
+});
+
+// Route to handle the user deletion
+app.post('/deactivate', (req, res) => {
+  const username = req.body.username;
+
+  const sql = `UPDATE USERS SET APPROVED = 0 WHERE username=?;`;
+  connection.query(sql, [username], (err, result) => {
+      if (err) {
+          console.error('Error deleting user:', err);
+          res.status(500).send('Error deleting user');
+      } else {
+          console.log(`User '${username}' deleted`);
+          res.send(`User '${username}' deleted`);
+      }
+  });
+});
 
 
 
